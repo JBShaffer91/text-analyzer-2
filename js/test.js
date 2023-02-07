1a3a7e2f0f32001d55eb9241a231d90294b310b2
Justin Shaffer <justinbshaffer91@gmail.com>
2023-02-06 19:33:13 -0800

    test that removeWords can omit multiple words

--- js/test.js
+++ js/test.js
@@ -5,3 +5,11 @@ it('Should remove the word "muppeteer" from the text', () => {
   let filteredText = removeWords(text, wordsToRemove);
   expect(filteredText).toBe("Hello there, how are you today?");
 });
+
+// Test case 2: Tests that the function removes multiple words from the text
+it('Should remove multiple words from the text', () => {
+  let text = "Hello there muppeteer, zoinks how are you today?";
+  let wordsToRemove = ["zoinks", "muppeteer", "biffaroni", "loopdaloop"];
+  let filteredText = removeWords(text, wordsToRemove);
+  expect(filteredText).toBe("Hello there, how are you today?");
+});
